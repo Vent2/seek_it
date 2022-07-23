0d075c907dc84809fef875f6f4cf711658a992bb
Vent2 <ventura.c.gerald@gmail.com>
2022-07-22 22:47:03 -0400

    search FIlters

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { Flex, Box, Text, Input, Spinner, Icon, Select, Button} from '@chakra-ui/react';
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+
+const SearchFilters = () => {
+    return (
+        <div>
+            searchFilters
+        </div>
+    )
+}
+
+
+export default SearchFilters
